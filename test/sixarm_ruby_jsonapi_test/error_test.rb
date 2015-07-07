df4c392bac6cc1214151a064b2d821b3b7db4fc4
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:43 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_jsonapi_test/error_test.rb
+++ test/sixarm_ruby_jsonapi_test/error_test.rb
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
+require "minitest/autorun"
 Minitest::Test ||= MiniTest::Unit::TestCase
-require 'simplecov'
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_jsonapi'
+require "sixarm_ruby_jsonapi"
 
 describe JSONAPI::Error do
 
